7bd8e5674a0aed126417f49930fbf8341defad73
William Ventura <wrventura567@gmail.com>
2017-09-22 11:34:18 -0400

    first commit

--- zip-search/src/App.js
+++ zip-search/src/App.js
@@ -4,29 +4,70 @@ import './App.css';
 
 
 function City(props) {
-  return (<div></div>);
+  return (
+	<div className="panel panel-default">
+		<div className="panel-heading">City Name</div>	
+	    <div className="panel-body"></div>
+	</div>
+);
 }
 
 function ZipSearchField(props) {
-  return (<div></div>);
+  return (
+	<form onSubmit={props.onSubmit}>
+		<div className="form-group row">
+			<label >Zip Code:</label>
+			<input type="text" class="form-control" id="usr"
+				onChange={props.onChange}/>
+			<input type="submit" value="Submit" />
+		</div>	
+	</form>
+  );
 }
 
-
+function Extract(props) {
+	return (
+		<div>
+		{props.map(function (i) {
+			return <City />;
+		})}
+		</div>
+	);
+}
+	
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Zip Code Search</h2>
-        </div>
-        <ZipSearchField />
-        <div>
-          <City />
-          <City />
-        </div>
-      </div>
-    );
-  }
+	constructor(props) {
+		super(props);
+		this.state = {value: 'init'};
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	handleSubmit(event) {
+		var url = 'http://ctp-zip-api.herokuapp.com/zip/';
+		url += this.state.value;
+		var ob = fetch(url);
+	}
+	
+	handleChange(event) {
+		this.setState({value: event.target.value.toLowerCase()});
+	}
+
+	render() {
+	return (
+	  <div className="App">
+		<div className="App-header">
+		  <h2>Zip Code Search</h2>
+		</div>
+		<ZipSearchField 
+			onChange={this.handleChange}
+			onSubmit={this.handleSubmit}/>
+		<div>
+			<City />
+		</div>
+	  </div>
+	);
+	}
 }
 
 export default App;
